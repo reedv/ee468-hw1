9b49a3faef320febce51813afb4badbe150ad7bc
reedv <villanuevareed@gmail.com>
2016-09-09 12:24:22 -1000

    added comments and some refactoring

--- sshell.c
+++ sshell.c
@@ -13,8 +13,8 @@
 #include <sys/types.h>
 #define BUFFER_SIZE 80  // Includes the space for terminating null
 #define ARR_SIZE 80
-#define MAX_PIPES 1  // for now
-#define MAX_CHILDS MAX_PIPES+1
+#define READ 0  // filedes pipe sides
+#define WRITE 1
 
 pid_t pid;
 
@@ -121,6 +121,9 @@ void parse_args(char *buffer, char** args,
 * Method for launching a program.
 */
 void commandRunner(char **args){
+#ifdef DEBUG
+	printf("**Entered commandRunner\n");
+#endif
 	 int err = -1;
 
 	 if((pid=fork()) == -1){
@@ -132,13 +135,29 @@ void commandRunner(char **args){
 		/************************************
 		 * Child Logic
 		 * **********************************/
-		// We set the child to ignore SIGINT signals (we want the parent
-		// process to handle them with signalHandler_int)
-		signal(SIGINT, SIG_IGN);
-
+//		// Set child to ignore SIGINT signals
+//		signal(SIGINT, SIG_IGN);
 
 		if (execvp(args[0], args) == err){
-			// If launch non-existing commands, end process
+			/*
+			 * The execvp function is similar to execv, except that it
+			 * searches the directories listed in the PATH environment variable
+			 * to find the full file name of a file from filename if filename
+			 * does not contain a slash.
+			 *
+			 * The argv argument is an array of null-terminated strings that
+			 * is used to provide a value for the argv argument to the main
+			 * function of the program to be executed. The last element of this
+			 * array must be a null pointer. By convention, the first element of
+			 * this array is the file name of the program sans directory names.
+			 * , for full details on how programs can access these arguments.
+			 *
+			 * This function is useful for executing system utility programs, because
+			 * it looks for them in the places that the user has chosen.
+			 * Shells use it to run the commands that users type.
+			 * */
+
+			// If tried to launch non-existing commands, end process
 			printf("Command not found");
 			kill(getpid(),SIGTERM);
 		}
@@ -159,27 +178,30 @@ void commandRunner(char **args){
 * Method used to manage pipes.
 */
 void pipeHandler(char * args[]){
+#ifdef DEBUG
+	printf("**Entered pipeHandler\n");
+#endif
 	// File descriptors
 	int filedes[2]; // pos.0 output, pos.1 input of the pipe
 	int filedes2[2];
 
-	int num_cmds = 0;
+	int num_cmds = 0;  // tracks commands for piped seq
+
 
 	char *command[256];
 
-	pid_t pid;
+//	pid_t pid;  // Have declared this as global, but kept this here in case something breaks down the line
 
 	int err = -1;
 	int end = 0;
 
-	// Variables used for the different loops
-	int i = 0;
-	int j = 0;
-	int k = 0;
-	int l = 0;
+	// Variables for loops
+	int i = 0,  // tracks which commands we are at in a seq. of piped commands
+		j = 0,  // tracks each arg between set of pipes
+		k = 0,  // tracks each arg in an aux. array of string ptrs.for commands between pipes
+		l = 0;  // tracks amount of pipes '|' in a command seq.
 
-	// First we calculate the number of commands (they are separated
-	// by '|')
+	// Calculate the number of commands (separated by '|')
 	while (args[l] != NULL){
 		if (strcmp(args[l],"|") == 0){
 			num_cmds++;
@@ -187,111 +209,132 @@ void pipeHandler(char * args[]){
 		l++;
 	}
 	num_cmds++;
+	const last_cmd_index = num_cmds-1;
 
-	// Main loop of this method. For each command between '|', the
-	// pipes will be configured and standard input and/or output will
-	// be replaced. Then it will be executed
+	// For each command between '|', configure pipes and set input/output.
+	// Then execute
 	while (args[j] != NULL && end != 1){
-		k = 0;
-		// We use an auxiliary array of pointers to store the command
-		// that will be executed on each iteration
+		k = 0;  // tracks args positions for each command
+				// in the command[] array of string ptrs.
+
+		// While the arg is not a pipe
 		while (strcmp(args[j],"|") != 0){
+			// Auxiliary array of pointers used to store the commands
+			// for execution on each iteration
 			command[k] = args[j];
+
 			j++;
+			// If no args would be found in a next iteration
 			if (args[j] == NULL){
-				// 'end' variable used to keep the program from entering
-				// again in the loop when no more arguments are found
-				end = 1;
+				end = 1;  // flag to not loop again
 				k++;
 				break;
 			}
 			k++;
 		}
-		// Last position of the command will be NULL to indicate that
+		// Last position, k, of the command needs to be NULL to indicate that
 		// it is its end when we pass it to the exec function
 		command[k] = NULL;
 		j++;
 
-		// Depending on whether we are in an iteration or another, we
-		// will set different descriptors for the pipes inputs and
-		// output. This way, a pipe will be shared between each two
-		// iterations, enabling us to connect the inputs and outputs of
-		// the two different commands.
+		// Depending on if we are in one iteration or another, set different
+		// descriptors for the pipes inputs and output. Thus, a pipe will be
+		// shared between each two iterations, so the inputs and outputs of
+		// the two different commands can communicate with each other.
+
+		// odd i
 		if (i % 2 != 0){
-			pipe(filedes); // for odd i
+			pipe(filedes);
+		// even i
 		}else{
-			pipe(filedes2); // for even i
+			pipe(filedes2);
 		}
 
 		pid=fork();
 
-		if(pid==-1){
+		if(pid==err){
 			if (i != num_cmds - 1){
+				// for odd i
 				if (i % 2 != 0){
-					close(filedes[1]); // for odd i
+					close(filedes[WRITE]);
+				// for even i
 				}else{
-					close(filedes2[1]); // for even i
+					close(filedes2[WRITE]);
 				}
 			}
 			printf("Child process could not be created\n");
 			return;
 		}
 		if(pid==0){
-			// If we are in the first command
+			/************************
+			 * Child Logic
+			 * **********************/
+			// If we are in very first command
 			if (i == 0){
-				dup2(filedes2[1], STDOUT_FILENO);
+				/*This function copies the descriptor old to descriptor number new.*/
+				dup2(filedes2[WRITE], STDOUT_FILENO);
 			}
 			// If we are in the last command, depending on whether it
-			// is placed in an odd or even position, we will replace
-			// the standard input for one pipe or another. The standard
-			// output will be untouched because we want to see the
-			// output in the terminal
+			// is placed in an odd or even position, replace
+			// the standard input for one pipe or another.
+			// Don't change stdout, since want to see output in terminal
 			else if (i == num_cmds - 1){
 				if (num_cmds % 2 != 0){ // for odd number of commands
-					dup2(filedes[0],STDIN_FILENO);
+					dup2(filedes[READ], STDIN_FILENO);
 				}else{ // for even number of commands
-					dup2(filedes2[0],STDIN_FILENO);
+					dup2(filedes2[READ], STDIN_FILENO);
 				}
-			// If we are in a command that is in the middle, we will
-			// have to use two pipes, one for input and another for
+
+			// If we are in some command in the middle of pipe seq.,
+			// have to use two pipes, one for input and one for
 			// output. The position is also important in order to choose
 			// which file descriptor corresponds to each input/output
-			}else{ // for odd i
+
+		    // for odd i
+			}else{
 				if (i % 2 != 0){
-					dup2(filedes2[0],STDIN_FILENO);
-					dup2(filedes[1],STDOUT_FILENO);
+					dup2(filedes2[READ], STDIN_FILENO);
+					dup2(filedes[WRITE], STDOUT_FILENO);
 				}else{ // for even i
-					dup2(filedes[0],STDIN_FILENO);
-					dup2(filedes2[1],STDOUT_FILENO);
+					dup2(filedes[READ], STDIN_FILENO);
+					dup2(filedes2[WRITE], STDOUT_FILENO);
 				}
 			}
 
 			if (execvp(command[0],command)==err){
-				kill(getpid(),SIGTERM);
+				kill(getpid(), SIGTERM);
 			}
 		}
 
-		// CLOSING DESCRIPTORS ON PARENT
+		/************************
+		 * Parent Logic
+		 * **********************/
+		// close parent file descriptors
 		if (i == 0){
-			close(filedes2[1]);
+			close(filedes2[WRITE]);
 		}
 		else if (i == num_cmds - 1){
 			if (num_cmds % 2 != 0){
-				close(filedes[0]);
+				close(filedes[READ]);
 			}else{
-				close(filedes2[0]);
+				close(filedes2[READ]);
 			}
 		}else{
 			if (i % 2 != 0){
-				close(filedes2[0]);
-				close(filedes[1]);
+				close(filedes2[READ]);
+				close(filedes[WRITE]);
 			}else{
-				close(filedes[0]);
-				close(filedes2[1]);
+				close(filedes[READ]);
+				close(filedes2[WRITE]);
 			}
 		}
 
-		waitpid(pid,NULL,0);
+		waitpid(pid, NULL, 0);
+		/*
+		 * The waitpid function is used to request status information from a child process
+		 * whose process ID is pid. Normally, the calling process is suspended until the
+		 * child process makes status information available by terminating.
+		 * */
 
 		i++;
 	}
@@ -301,34 +344,29 @@ void pipeHandler(char * args[]){
 * Method used to handle the commands entered via the standard input
 */
 int commandHandler(char * args[]){
+#ifdef DEBUG
+	printf("**Entered commandHandler\n");
+#endif
 	int i = 0;
 	int j = 0;
 
-	int background = 0;
-
-	// 'exit' command quits the shell
+	/* Check for some common/basic commands */
+	// 'exit' command quits shell
 	if(strcmp(args[0],"exit") == 0) exit(0);
- 	// 'clear' command clears the screen
+ 	// 'clear' command clears screen
 	else if (strcmp(args[0],"clear") == 0) system("clear");
-//	// 'cd' command to change directory
-//	else if (strcmp(args[0],"cd") == 0) changeDirectory(args);
 	else{
-		// If none of the preceding commands were used, we invoke the
-		// specified program. Detect if piped execution was used.
+		// Check if pipes used
 		while (args[i] != NULL){
-			// If '|' is detected, piping was solicited, and we call
-			// the appropriate method that will handle the different
-			// executions
 			if (strcmp(args[i],"|") == 0){
 				pipeHandler(args);
 				return 1;
 			}
 			i++;
 		}
-		// We launch the program with our method, indicating if we
-		// want background execution or not
-		commandRunner(args);
+		commandRunner(args);  // (char *args[]) == (char **args), since c-arrays are ptrs.
 	}
+
 return 1;
 }
 
@@ -362,8 +400,6 @@ int main(int argc, char *argv[], char *envp[]){
  
         /* Nothing entered so prompt again */
         if (nargs==0) continue;
-        /* special 'exit' command entered, exit shell */
-        if (strcmp(args[0], "exit" )==0) exit(0);
 
         //Count number of pipes.
         int pipe_count=0;
